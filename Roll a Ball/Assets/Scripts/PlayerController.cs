95a6373de24e7aff8d137ab934a58d8a526e0356
vividmuimui <tkoyama@aiming-inc.com>
2016-11-05 21:15:29 +0900

    [Roll a Ball]finish 1-2

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movoment = new Vector3(moveHorizontal, 0, moveVertical);
+		
+		rb.AddForce(movoment * speed);
+	}
+}
