fd3f60344081bf7830870d116c4b7b27851f9af3
vividmuimui <tkoyama@aiming-inc.com>
2016-11-24 06:34:38 +0900

    [Roguelike]finish 3-4

--- Roguelike/Assets/Scripts/GameManager.cs
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class GameManager : MonoBehaviour {
 	public static GameManager instance = null;
 	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
 	private int level = 3;
 
 	void Awake () 
@@ -25,8 +27,12 @@ public class GameManager : MonoBehaviour {
 	{
 		boardScript.SetupScene(level);
 	}
+
+	public void GameOver()
+	{
+		enabled = false;
+	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
