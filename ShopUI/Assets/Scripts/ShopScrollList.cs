847f8ccff906387323052ce0a4e0b800fff79713
vividmuimui <tkoyama@aiming-inc.com>
2016-11-26 07:55:55 +0900

    [ShopUI]finish 5

---
+++ ShopUI/Assets/Scripts/ShopScrollList.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item 
+{
+	public string itemName;
+	public Sprite icon;
+	public float price = 1f;
+}
+
+public class ShopScrollList : MonoBehaviour {
+	public List<Item> itemList;
+	public Transform contentPanel;
+	public ShopScrollList otherShop;
+	public Text myGoldDisplay;
+	public SimpleObjectPool ButtonObjectPool;
+	public float gold = 20f;
+
+	void Start () {
+	
+	}
+	
+	private void AddButton()
+	{
+		for(int i = 0; i < itemList.Count; i++)
+		{
+			Item item = itemList[i];
+			GameObject newButton = ButtonObjectPool.GetObject();
+		}
+	}
+}
