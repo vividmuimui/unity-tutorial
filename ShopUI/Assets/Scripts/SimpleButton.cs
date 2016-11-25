8662779c14878864920a755fc66128fadfc30577
vividmuimui <tkoyama@aiming-inc.com>
2016-11-26 07:33:19 +0900

    [ShopUI]finish 4

---
+++ ShopUI/Assets/Scripts/SimpleButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleButton : MonoBehaviour {
+	public Button button;
+	public Text nameLabel;
+	public Text priceLabel;
+	public Image iconImage;
+
+	void Start () 
+	{
+	
+	}
+
+	public void Setup()
+	{
+		
+	}
+}
