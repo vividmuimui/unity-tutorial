e903a61baed3e088802239c23ea3e399d51f0e8e
vividmuimui <tkoyama@aiming-inc.com>
2016-11-27 03:34:42 +0900

    [CreatingBasicEditorTools] finish CreateCharacterWizard

---
+++ Creating Basic Editor Tools/Assets/Editor/CreateCharacterWizard.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateCharacterWizard : ScriptableWizard {
+	public Texture2D portraitTexture;
+	public Color color = Color.white;
+	public string nickname = "Default nickname";
+	
+	[MenuItem ("My Tools/Create character wizard...")]
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard<CreateCharacterWizard>(
+			"Create Character", 
+			"Create new", 
+			"Update Selected"
+		);
+	}
+
+	void OnWizardCreate()
+	{
+		GameObject characterGO = new GameObject();
+		Character characterComponent = characterGO.AddComponent<Character>();
+		characterComponent.portrait = portraitTexture;
+		characterComponent.nickname = nickname;
+		characterComponent.color = color;
+
+		PlayerMovement playerMovement = characterGO.AddComponent<PlayerMovement>();
+		characterComponent.playerMovement = playerMovement;
+	}
+
+	void OnWizardOtherButton()
+	{
+		if(Selection.activeTransform != null)
+		{
+			Character characterComponent = Selection.activeTransform.GetComponent<Character>();
+			if(characterComponent != null)
+			{
+				characterComponent.portrait = portraitTexture;
+				characterComponent.nickname = nickname;
+				characterComponent.color = color;
+			}
+		}
+	}
+
+	void OnWizardUpdate()
+	{
+		helpString = "Enter character details";
+		errorString = "test error string";
+	}
+}
---
+++ Creating Basic Editor Tools/Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	public Texture2D portrait;
+	public string nickname;
+	public Color color;
+
+	public PlayerMovement playerMovement;
+}
